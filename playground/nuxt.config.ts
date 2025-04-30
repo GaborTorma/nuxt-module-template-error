e5e77e230893cee45c7cc18bd28361d6f18b0b02
Gábor Torma <gabor@torma.dev>
2025-04-30 22:06:28 +0200

    add issue comments

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,10 +1,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-04-30',
 
-  // buildDir: './.nuxt',
+  // buildDir: './.nuxt', // uncomment it and no build error, because buildDir not in node_modules
 
   future: {
-    compatibilityVersion: 4,
+    compatibilityVersion: 4, // change to 3 and no build error, because the buildDir is .nuxt. Doesn't changed to node_modules/.cache/nuxt/.nuxt
   },
 
   extends: [
